45027daddc8eae4add28a64110133c156b36d6c5
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-10-17 15:58:16 -0700

    tested CREATE operation by adding mock jobs info in server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 
 import morgan from "morgan";
 
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -20,6 +27,10 @@ app.post("/", (req, res) => {
   console.log(req);
 
   res.json({ message: "Data received", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
