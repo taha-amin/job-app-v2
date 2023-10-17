0a9d063741ca1fa2d87583fdab493e450b9e2d75
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-10-17 13:48:00 -0700

    setup express middleware to accept json

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
 });
 
 app.listen(5100, () => {
