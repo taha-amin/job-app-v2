164019115e16b86bf1ede2e83cb2f6523be8987e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-12-19 16:07:47 -0800

    Created Job schema and connected to mongoDB

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,5 @@
+import Job from "../models/jobModel.js";
+
 import { nanoid } from "nanoid";
 
 let jobs = [
@@ -13,13 +15,7 @@ export const getAllJobs = async (req, res) => {
 //CREATE JOB
 export const createJob = async (req, res) => {
   const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create({ company, position });
   res.status(201).json({ job });
 };
 
