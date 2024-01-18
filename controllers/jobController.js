1c03250c52d591470bc6c3e6f9e13d9b773dadf8
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-01-18 13:47:49 -0800

    Added functionality for updating job in list

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,5 @@
 import Job from "../models/jobModel.js";
 
-import { nanoid } from "nanoid";
-
-let jobs = [
-  { id: nanoid(), company: "apple", position: "front-end" },
-  { id: nanoid(), company: "google", position: "back-end" },
-];
-
 //GET JOBS
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -33,20 +26,17 @@ export const getJob = async (req, res) => {
 
 //UPDATE JOB
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
 
-  if (!job) {
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!updatedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: "job modified", job });
+  res.status(200).json({ job: updatedJob });
 };
 
 //DELETE JOB
