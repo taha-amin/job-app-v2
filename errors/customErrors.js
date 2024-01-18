a6c373e3ab887858aabdb0e1f7b09a39d822c2eb
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-01-18 14:48:06 -0800

    added bad request errors to customErrors.js

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,5 +1,33 @@
 import { StatusCodes } from "http-status-codes";
 
+//400 - BAD REQUEST
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BadRequestError";
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+//401 - UNAUTHORIZED
+export class UnauthenticatedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UnauthenticatedError";
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+//403 - FORBIDDEN
+export class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UnauthorizedError";
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
+
+//404 - NOT FOUND
 export class NotFoundError extends Error {
   constructor(message) {
     super(message);
