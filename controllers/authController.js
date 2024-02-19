a2cd1b29c772f4be26e8b002f142ce1ca0d6a408
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-02-19 13:54:00 -0800

    imported and utilized bcryptjs to hash passwords

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,18 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
+  //a random value that is added to the password before hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
