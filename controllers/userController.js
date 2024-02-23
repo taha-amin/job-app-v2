4a45d5b02954cfc7d013c8768c28d2b6dfa7adce
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-02-22 16:12:08 -0800

    Wrote functionality for user updating their info & for their password

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,5 +14,8 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "update user" });
+  const obj = { ...req.body };
+  delete obj.password;
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
+  res.status(StatusCodes.OK).json({ msg: "user updated" });
 };
