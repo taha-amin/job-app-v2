347cbc511ca0a914edc5b1f685fd3d7b2c13bf91
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-02-22 16:23:13 -0800

    setup functionality for admin role seeing application stats of web app

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,3 +17,12 @@ export const authenticateUser = (req, res, next) => {
     throw new UnauthenticatedError("authentication invalid");
   }
 };
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthenticatedError("Unauthorized to access this route");
+    }
+    next();
+  };
+};
