ceb99199636ef6ecba985844851d801222457594
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-04-15 16:10:45 -0700

    Created functionality for adding job and redirecting to alljobs page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import {
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
+import { action as addJobAction } from "./pages/AddJob";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const checkDefaultTheme = () => {
@@ -54,6 +55,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <AddJob />,
+            action: addJobAction,
           },
           {
             path: "stats",
