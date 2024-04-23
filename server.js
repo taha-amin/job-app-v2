489ead3fd08aefe44d870e8449a9890d8bd03ec4
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-04-22 17:22:45 -0700

    Installed & setup instance of cloudinary package (cloud-based media management platform that helps developers with an easy way to upload pictures enabling faster and more efficient delivery of visual content on websites and applications). Updated controllers.userController.js to import cloudinary & include necessary codes to allow updated user to show profile picture. Updated pages/LogoutContainer.jsx to include user avatar in web application.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
 
 // ROUTERS
 import jobRouter from "./routes/jobRouter.js";
@@ -23,6 +24,12 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 // MIDDLEWARE
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
