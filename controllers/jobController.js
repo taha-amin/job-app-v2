dc64f8dbd2026bbbc3565c0e85bde9e5a37571f8
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-04-29 14:37:11 -0700

    The diff modifies getAllJobs function in controllers/jobController.js to add search functionality. It introduces a search parameter from the request query and constructs a queryObject based on the createdBy field and optional search terms. If a search term is provided, it filters jobs by either the position or company name using a case-insensitive regex search. This enhances the function to allow filtering jobs based on user input.

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,7 +5,20 @@ import day from "dayjs";
 
 //GET JOBS
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const { search } = req.query;
+
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+
+  if (search) {
+    queryObject.$or = [
+      { position: { $regex: search, $options: "i" } },
+      { company: { $regex: search, $options: "i" } },
+    ];
+  }
+
+  const jobs = await Job.find(queryObject);
 
   res.status(StatusCodes.OK).json({ jobs });
 };
