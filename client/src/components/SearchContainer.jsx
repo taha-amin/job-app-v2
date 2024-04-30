e77fce75844d41e7116317b2dd587e6e9df39038
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-04-30 13:40:09 -0700

    feat(SearchContainer): add debounce to search input
    A debounce function has been introduced to the search input field in the SearchContainer component. This function delays the form submission by 2000 milliseconds after the user stops typing, which helps in reducing the number of submit calls made during search input. The debounce function wraps the existing submit function call, ensuring that it only triggers after the specified delay.

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -10,6 +10,17 @@ const SearchContainer = () => {
   const { search, jobStatus, jobType, sort } = searchValues;
   const submit = useSubmit();
 
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 2000);
+    };
+  };
+
   return (
     <Wrapper>
       <Form className="form">
@@ -21,9 +32,9 @@ const SearchContainer = () => {
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
 
           <FormRowSelect
