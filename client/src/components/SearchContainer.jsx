d2c0ee814ae32d399f49252fefb111cb023ee76a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-04-30 13:17:35 -0700

    feat(SearchContainer): enhance form components with dynamic values
    - Update  and  components to accept an  prop to handle form changes dynamically.
    - Modify  to utilize context values () for default values in form fields and to submit the form on change.
    - Ensure that the  for each form field in  is set based on the current search context, allowing for a more responsive and user-specific interface.
    - Remove temporary  component usage as form submission is now handled via the  event.

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -6,6 +6,10 @@ import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from "../../../utils/constants";
 import { useAllJobsContext } from "../pages/AllJobs";
 
 const SearchContainer = () => {
+  const { searchValues } = useAllJobsContext();
+  const { search, jobStatus, jobType, sort } = searchValues;
+  const submit = useSubmit();
+
   return (
     <Wrapper>
       <Form className="form">
@@ -13,33 +17,47 @@ const SearchContainer = () => {
         <div className="form-center">
           {/* search position */}
 
-          <FormRow type="search" name="search" defaultValue="a" />
+          <FormRow
+            type="search"
+            name="search"
+            defaultValue={search}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
+          />
 
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
             list={["all", ...Object.values(JOB_STATUS)]}
-            defaultValue="all"
+            defaultValue={jobStatus}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
 
           <FormRowSelect
             labelText="job type"
             name="jobType"
             list={["all", ...Object.values(JOB_TYPE)]}
-            defaultValue="all"
+            defaultValue={jobType}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
 
           <FormRowSelect
             name="sort"
-            defaultValue="newest"
+            defaultValue={sort}
             list={[...Object.values(JOB_SORT_BY)]}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
 
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
           </Link>
-          {/* TEMP!!! */}
-          <SubmitBtn formBtn />
         </div>
       </Form>
     </Wrapper>
