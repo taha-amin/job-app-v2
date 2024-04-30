e8fb36edfb63fc46aabcd7e8cfcc5757bc6bb283
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-04-30 15:49:09 -0700

    feat(JobsContainer): integrate PageBtnContainer for pagination
    - Import and use PageBtnContainer in JobsContainer to handle job listing pagination.
    - Update the data destructuring in JobsContainer to include totalJobs and numOfPages from the context.
    - Display the total number of jobs found above the job listings.
    - Conditionally render the PageBtnContainer based on the numOfPages, only showing it when more than one page is available.

---
+++ client/src/components/PageBtnContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useAllJobsContext } from "../pages/AllJobs";
+
+const PageBtnContainer = () => {
+  const {
+    data: { numOfPages, currentPage },
+  } = useAllJobsContext();
+
+  console.log(numOfPages, currentPage);
+
+  return <h1>PageBtnContainer</h1>;
+};
+
+export default PageBtnContainer;
