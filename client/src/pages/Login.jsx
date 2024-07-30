0f7e0c63077964ee4d04ddab68dc73446f36032a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2024-07-30 16:10:05 -0700

    feat(query): integrate QueryClient for cache invalidation
    - Updated  and  to accept  as a parameter in  to enable cache invalidation.
    - Modified  to invalidate all queries upon user logout.
    - Enhanced  action to invalidate queries after a successful login.
    - Improved  action to invalidate the user query after updating the user profile and added a file size check for the avatar upload.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,20 +3,24 @@ import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Link, Form, redirect, useNavigate } from "react-router-dom";
 import customFetch from "../../../utils/customFetch";
 import { toast } from "react-toastify";
+import { QueryClient } from "@tanstack/react-query";
 
-export const action = async ({ request }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
+export const action =
+  (QueryClient) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
 
-  try {
-    await customFetch.post("/auth/login", data);
-    toast.success("Login Successful!");
-    return redirect("/dashboard");
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
-  }
-};
+    try {
+      await customFetch.post("/auth/login", data);
+      QueryClient.invalidateQueries();
+      toast.success("Login Successful!");
+      return redirect("/dashboard");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return error;
+    }
+  };
 
 const Login = () => {
   const navigate = useNavigate();
